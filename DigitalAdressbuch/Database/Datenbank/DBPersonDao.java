87548ee7c29eade0d1563838565cff9f382b2157
zmeieb <zmeieb@WS-ZMEIEB.bbcnet-zh.local>
2018-02-06 13:36:16 +0100

    added database

---
+++ DigitalAdressbuch/Database/Datenbank/DBPersonDao.java
@@ -0,0 +1,10 @@
+package Datenbank;
+
+import java.util.List;
+
+public interface DBPersonDao {
+	
+	public abstract void insertPerson(DBPerson p);
+	public abstract DBPerson findPersonById(String vorname);
+	public abstract List<DBPerson> getAllPersons();
+}
